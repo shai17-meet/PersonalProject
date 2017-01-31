39c9f9727272c9ab7c6354052244ae3a5c4db527
einav18-meet <einavcohen48@gmail.com>
2017-01-31 19:20:13 +0200

    check

--- webapp.py
+++ webapp.py
@@ -65,19 +65,23 @@ def homepage():
 
 @app.route('/newThread', methods = ['GET','POST'])
 def newThread():
-    if request.method == 'POST':
-        thread_title = request.form['title']
-        thread_content = request.form['thread_content']
-        if thread_title is None or thread_content is None:
-            flash("Your form is missing arguments")
-            return redirect(url_for('newThread'))
-        thread =Thread(title = thread_title, text= thread_content)
-        session.add(thread)
-        session.commit()
-        flash("Thread Added Successfully!")
-        return redirect(url_for('threads'))
-    else:
-		return render_template('newThread.html')
+	if 'id' in login_session:
+	    if request.method == 'POST':
+	        thread_title = request.form['title']
+	        thread_content = request.form['thread_content']
+	        if thread_title is None or thread_content is None:
+	            flash("Your form is missing arguments")
+	            return redirect(url_for('newThread'))
+	        thread =Thread(title = thread_title, text= thread_content, user_id=login_session['id'])
+	        session.add(thread)
+	        session.commit()
+	        flash("Thread Added Successfully!")
+	        return redirect(url_for('threads'))
+	    else:
+			return render_template('newThread.html')
+	else:
+		flash("You must log in to view this page")
+		return redirect(url_for('login'))
 
 @app.route('/threads')
 def threads():
@@ -89,6 +93,16 @@ def threadpage(thread_id):
 	thread= session.query(Thread).filter_by(id=thread_id).one()
 	return render_template('threadpage.html', thread=thread)
 
+@app.route('/logout')
+def logout():
+	if 'id' not in login_session:
+		flash("You must be logged in to be able to log out")
+		return redirect(url_for('login'))
+	del login_session['name']
+	del login_session['email']
+	del login_session['id']
+	flash("logged out successfully")
+	return redirect(url_for('homepage'))
 
 
 
